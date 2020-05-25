1abb12264ed8bfc2532c854f13d9478239f4be28
Thomas Cyron <thomas@thcyron.de>
2020-05-25 10:52:11 +0200

    Add reconcile.DaemonSet()

---
+++ reconcile/daemon_set.go
@@ -0,0 +1,32 @@
+package reconcile
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/ericchiang/k8s"
+	appsv1 "github.com/ericchiang/k8s/apis/apps/v1"
+
+	"github.com/thcyron/skop/skop"
+)
+
+func DaemonSet(ctx context.Context, client skop.Client, expected *appsv1.DaemonSet) error {
+	existing := new(appsv1.DaemonSet)
+	err := client.Get(
+		ctx,
+		expected.GetMetadata().GetName(),
+		existing,
+	)
+	if err != nil {
+		if apiErr, ok := err.(*k8s.APIError); ok {
+			if apiErr.Code == http.StatusNotFound {
+				return client.Create(ctx, expected)
+			}
+		}
+		return err
+	}
+	existing.Metadata.Labels = expected.Metadata.Labels
+	existing.Metadata.Annotations = expected.Metadata.Annotations
+	existing.Spec = expected.Spec
+	return client.Update(ctx, existing)
+}
